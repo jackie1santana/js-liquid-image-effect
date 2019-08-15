d497d691a08de116104d4e22c13298f0f01c1d6e
Jackie Santana <jackiebrown.nyc@gmail.comgit config --global user.email jackiebrown.nyc@gmail.com>
2019-08-15 15:10:00 -0500

    liquid images

---
+++ app.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: './bw1.jpg',
+    image2: './bw2.jpg',
+    angle: Math.PI / 4,
+    displacementImage: './diss.png'
+})
